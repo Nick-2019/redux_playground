78ad5b9349343bba1a619aa8308f5218f44284a5
github username <github email address>
2019-10-09 23:41:27 -0500

    time to eat beans

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useSelector} from 'react-redux'
 // import 
 // import { createStore } from 'react-redux'
 
 function App() {
+
+  const counter = useSelector(state => state.counter)
+  const logged = useSelector(state => state.login)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>You currently have 0 beans!</h1>
+        <h1>You currently have {counter} can of beans!</h1>
+        <div>
+        <button>Buy Beans</button>
+        <button>Eat Beans</button>
+        </div>
+
+        {logged === true ? <h3>This should not appear when not logged in</h3> : ''}
       </header>
     </div>
   );
