0c8a616f13b93286b72b0b1ced00d5729c116933
github username <github email address>
2019-10-09 23:28:13 -0500

    Redux Devtool

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
 import allReducer from './reducers'
 
-const store = createStore(allReducer);
+const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
